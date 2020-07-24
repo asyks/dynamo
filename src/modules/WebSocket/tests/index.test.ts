bd590a9be240450f7c0be2dc5aa00ceb8d5e461a
asyks <aaron@aaronsykes.com>
2020-07-23 20:07:41 -0700

    Add more websocket module tests

--- src/modules/WebSocket/tests/index.test.ts
+++ src/modules/WebSocket/tests/index.test.ts
@@ -1,21 +1,74 @@
-import { fireEvent } from '@testing-library/react'
 import AttentedWebSocket, { messageEventWrapper } from '../index'
 
+let websocket: AttentedWebSocket
+let eventCallback: jest.Mock
+
 describe("modules/WebSocket", () => {
-  it("test handles events", () => {
-    const eventCallback = jest.fn((message: string) => { })
-    const websocket = new AttentedWebSocket(
+
+  beforeEach(() => {
+    eventCallback = jest.fn((message: string) => { })
+    websocket = new AttentedWebSocket(
       "wss://foo.bar",
       evt => { messageEventWrapper(evt, eventCallback) },
     )
+  })
 
-
+  it("handles simple event message sequence", () => {
     websocket.dispatchEvent(new Event("open"))
-    websocket.dispatchEvent(new MessageEvent("message", { data: "test message" }))
+    websocket.dispatchEvent(
+      new MessageEvent(
+        "message",
+        { data: "test message" },
+      )
+    )
     websocket.dispatchEvent(new Event("close"))
 
     expect(eventCallback).toHaveBeenCalledWith("CONNECTED")
     expect(eventCallback).toHaveBeenCalledWith("RECEIVED: test message")
+    expect(eventCallback).toHaveBeenCalledWith("DISCONNECTED")
+  })
+
+  it("handles multiple event message sequence", () => {
+    websocket.dispatchEvent(new Event("open"))
+    websocket.dispatchEvent(
+      new MessageEvent(
+        "message",
+        { data: "first test message" },
+      )
+    )
+    websocket.dispatchEvent(
+      new MessageEvent(
+        "message",
+        { data: "second test message" },
+      )
+    )
+    websocket.dispatchEvent(
+      new MessageEvent(
+        "message",
+        { data: "third test message" },
+      )
+    )
+    websocket.dispatchEvent(new Event("close"))
+
+    expect(eventCallback).toHaveBeenCalledWith("CONNECTED")
+    expect(eventCallback).toHaveBeenCalledWith("RECEIVED: first test message")
+    expect(eventCallback).toHaveBeenCalledWith("RECEIVED: second test message")
+    expect(eventCallback).toHaveBeenCalledWith("RECEIVED: third test message")
+    expect(eventCallback).toHaveBeenCalledWith("DISCONNECTED")
+  })
+
+  it("handles error event sequence", () => {
+    websocket.dispatchEvent(new Event("open"))
+    websocket.dispatchEvent(
+      new MessageEvent(
+        "error",
+        { data: "test error" },
+      )
+    )
+    websocket.dispatchEvent(new Event("close"))
+
+    expect(eventCallback).toHaveBeenCalledWith("CONNECTED")
+    expect(eventCallback).toHaveBeenCalledWith("ERROR: test error")
     expect(eventCallback).toHaveBeenCalledWith("DISCONNECTED")
   })
 })
