072b0c3a0529311bce60b9947e3f24a0090c44bd
asyks <aaron@aaronsykes.com>
2020-08-22 22:23:46 -0700

    Add initial helper function test

---
+++ src/modules/Ndt/tests/utils.test.ts
@@ -0,0 +1,37 @@
+import { getServiceData, parseSecureServiceUrls } from '../utils'
+import { LocateApiV2Result } from '../types'
+
+describe("Ndt/utils.parseServiceUrls", () => {
+  test("parses urls from locate api result", () => {
+    const result: LocateApiV2Result = {
+      machine: "mlab2-lga05.mlab-oti.measurement-lab.org",
+      location: {
+        city: "New York",
+        country: "US"
+      },
+      urls: {
+        "wss:///ndt/v7/download": (
+          "wss://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
+        ),
+        "wss:///ndt/v7/upload": (
+          "wss://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
+        ),
+        "ws:///ndt/v7/download": (
+          "ws://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
+        ),
+        "ws:///ndt/v7/upload": (
+          "ws://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
+        ),
+      }
+    }
+
+    expect(parseSecureServiceUrls(result)).toBe({
+      "wss:///ndt/v7/download": (
+        "wss://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
+      ),
+      "wss:///ndt/v7/upload": (
+        "wss://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
+      ),
+    })
+  })
+})
