ffb0c76be89c4f75ab11504b45b03f21d45f3099
asyks <aaron@aaronsykes.com>
2020-08-23 22:13:03 -0700

    Add locate api respsonse test

--- src/modules/Ndt/tests/utils.test.ts
+++ src/modules/Ndt/tests/utils.test.ts
@@ -1,31 +1,52 @@
-import { getServiceData, parseSecureServiceUrls } from '../utils'
+import * as requests from '../requests'
 import { LocateApiV2Result } from '../types'
+import { getServiceUrls, parseSecureServiceUrls } from '../utils'
+
+jest.mock("../requests")
+
+const result: LocateApiV2Result = {
+  machine: "mlab2-lga05.mlab-oti.measurement-lab.org",
+  location: {
+    city: "New York",
+    country: "US"
+  },
+  urls: {
+    "wss:///ndt/v7/download": (
+      "wss://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
+    ),
+    "wss:///ndt/v7/upload": (
+      "wss://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
+    ),
+    "ws:///ndt/v7/download": (
+      "ws://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
+    ),
+    "ws:///ndt/v7/upload": (
+      "ws://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
+    ),
+  }
+}
 
 describe("Ndt/utils.parseServiceUrls", () => {
   test("parses urls from locate api result", () => {
-    const result: LocateApiV2Result = {
-      machine: "mlab2-lga05.mlab-oti.measurement-lab.org",
-      location: {
-        city: "New York",
-        country: "US"
-      },
-      urls: {
-        "wss:///ndt/v7/download": (
-          "wss://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
-        ),
-        "wss:///ndt/v7/upload": (
-          "wss://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
-        ),
-        "ws:///ndt/v7/download": (
-          "ws://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token="
-        ),
-        "ws:///ndt/v7/upload": (
-          "ws://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token="
-        ),
-      }
-    }
-
     expect(parseSecureServiceUrls(result)).toStrictEqual({
+      "download": (
+        new URL("wss://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token=")
+      ),
+      "upload": (
+        new URL("wss://ndt-mlab2-lga05.fake.org/ndt/v7/upload?access_token=")
+      ),
+    })
+  })
+})
+
+describe("Ndt/utils.getServiceUrls", () => {
+  beforeEach(() => {
+    const mockedAsyncGet = requests.asyncGet as jest.Mock
+    mockedAsyncGet.mockResolvedValue({ results: [result] })
+  })
+
+  test("extract urls from locate api response", async () => {
+    await expect(getServiceUrls()).resolves.toStrictEqual({
       "download": (
         new URL("wss://ndt-mlab2-lga05.fake.org/ndt/v7/download?access_token=")
       ),
