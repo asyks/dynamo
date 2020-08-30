d7302ea2df46a4a0e6b955032dcb7b16b286418e
asyks <aaron@aaronsykes.com>
2020-08-29 21:08:51 -0700

    Cleanup ndt requests module a bit

--- src/modules/Ndt/requests.ts
+++ src/modules/Ndt/requests.ts
@@ -4,8 +4,12 @@ export const asyncGet = async (url: URL): Promise<Object> => {
   return new Promise((resolve, reject) => {
     let respData: string = ""
     https.get(url.toString(), resp => {
-      resp.on("data", (chunk) => { console.log(resp); respData += chunk })
-      resp.on("end", () => { console.log(respData); resolve(JSON.parse(respData)) })
+      resp.on("data", (chunk) => {
+        respData += chunk
+      })
+      resp.on("end", () => {
+        resolve(JSON.parse(respData))
+      })
     }).on("error", (err) => {
       reject({ error: err.message })
     })
